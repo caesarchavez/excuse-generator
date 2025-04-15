873d54ded5d2fff4a453aac3191c420bdced6939
caesarchavez <107122259+caesarchavez@users.noreply.github.com>
2025-04-15 00:22:27 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ['My lights', 'My dog', 'My brother', 'My Gilfriend', 'My parents'];
+  let action = [' Went out', ' Ate', ' Stole', ' Destroyed', ' Threw Away']; 
+  let task = [' Lights', ' Homework', ' Tax Document', ' Work', ' Medicine']; 
+  let time = [' Today', ' Yesterday', ' This week', ' Tomorrow'];
+  
+  
+  let randomwho = Math.floor(Math.random() * who.length);
+  let randomaction = Math.floor(Math.random() * action.length);
+  let randomtask = Math.floor(Math.random() * task.length);
+  let randomtime = Math.floor(Math.random() * time.length);
+  
+  document.getElementById("excuse").innerText = who[randomwho] + action[randomaction] + task [randomtask] + time[randomtime];
+
+
 };
